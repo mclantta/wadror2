411bc396b58d10d3861b6836d2bc87581a55b797
Maija Lantta <maija.lantta@cs.helsinki.fi>
2016-03-05 17:49:52 +0200

    week7, assign. 9

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228163504) do
+ActiveRecord::Schema.define(version: 20160305141252) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -66,5 +66,7 @@ ActiveRecord::Schema.define(version: 20160228163504) do
     t.string   "password_digest"
     t.boolean  "admin"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
